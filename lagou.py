ed2fb3577220767403a825a133d931c7e7acfbbc
hanjiaxuan <597410004@qq.com>
2020-12-03 22:53:45 +0800

    拉勾专栏基本功能爬取完成

---
+++ lagou.py
@@ -0,0 +1,96 @@
+import asyncio
+import logging
+import os
+
+import aiohttp
+import requests
+from aiohttp import ContentTypeError
+from jinja2 import Environment, FileSystemLoader
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s: %(message)s')
+LESSION_URL = 'https://gate.lagou.com/v1/neirong/kaiwu/getCourseLessons?courseId={id}'
+INDEX_URL = 'https://gate.lagou.com/v1/neirong/kaiwu/getCourseLessonDetail?lessonId={id}'
+COURSE_ID = 287
+CONCURRENCY = 5
+
+loop = asyncio.get_event_loop()
+
+headers = {
+    # 在这里填入你的headers
+}
+
+
+class Spider(object):
+
+    def __init__(self):
+        self.session = aiohttp.ClientSession()
+        self.semaphore = asyncio.Semaphore(CONCURRENCY)
+
+    def scrape_lession_content(self, url):
+        url = url.format(id=COURSE_ID)
+        logging.info('scraping %s', url)
+        param = {'courseId': COURSE_ID}
+        r = requests.get(url, params=param, headers=headers)
+        return r.json()
+
+    async def scrape_api(self, url):
+        async with self.semaphore:
+            try:
+                logging.info('scraping %s', url)
+                params = {'lessonId': url.split('=')[1]}
+                async with self.session.get(url, headers=headers, params=params) as response:
+                    await asyncio.sleep(1)
+                    return await response.json()
+            except ContentTypeError as e:
+                logging.error('error occurred while scraping %s', url, exc_info=True)
+
+    async def scrape_detail(self, id):
+        url = INDEX_URL.format(id=id)
+        return await self.scrape_api(url)
+
+    # html名称,目录名称,pdf内容,pdf所在文件夹
+    async def write_html(self, file_name, theme, data, file_path):
+        logging.info('saving data %s,%s', file_name, data)
+        env = Environment(loader=FileSystemLoader('./'))
+        template = env.get_template('template.html')
+
+        if data:
+            xlsx_path = os.getcwd() + file_path
+            if not os.path.exists(xlsx_path):
+                os.makedirs(xlsx_path)
+            with open(xlsx_path + '/' + file_name, 'w+', encoding='utf-8') as fout:
+                html_content = template.render(theme=theme, body=data)
+                fout.write(html_content)
+
+    async def main(self):
+        response = self.scrape_lession_content(LESSION_URL)
+        course_section_list = response['content']['courseSectionList']
+        lession_id = []
+        file_path = response['content']['courseName']
+        for course in course_section_list:
+            for lession in course['courseLessons']:
+                lession_id.append(lession['id'])
+        scrape_index_tasks = [asyncio.ensure_future(self.scrape_detail(page)) for page in lession_id]
+        results = await asyncio.gather(*scrape_index_tasks)
+        print('results', results)
+        contents = []
+        for index_data in results:
+            content = index_data['content']
+            if content is None:
+                continue
+            data = {'id': int(content['id']), 'theme': str(content['theme']),
+                    'content': str(content['textContent'])}
+            contents.append(data)
+
+        scrape_detail_tasks = [asyncio.ensure_future(self.write_html(str(content['id']) + '.html',
+                                                                     content['theme'], content['content'],
+                                                                     '/' + file_path))
+                               for content in contents]
+        await asyncio.wait(scrape_detail_tasks)
+        await self.session.close()
+
+
+if __name__ == '__main__':
+    spider = Spider()
+    loop.run_until_complete(spider.main())
