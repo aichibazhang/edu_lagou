ae7745457fa4de61f4a11e87fec3d763482cb256
handd <597410004@qq.com>
2021-02-24 15:38:36 +0800

    [代码重构](master):
    功能完成：
    1. 多进程爬取（pdfkit机制使用该功能效率太低）
    2. 单进程转换pdf

--- htmltopdf.py
+++ htmltopdf.py
@@ -1,50 +1,62 @@
 import os
 from functools import cmp_to_key
+from multiprocessing.dummy import Pool
 
 import pdfkit
 
 
-def getAllSub(path):
-    dir_list = []
-    for home, dirs, files in os.walk(path):
-        # 获得所有文件夹
-        for dirname in dirs:
-            dir_names = os.path.join(home, dirname)
-            dir_flag = '.git' not in dir_names and '.idea' not in dir_names and 'venv' not in dir_names and 'login' not in dir_names and 'pdf' not in dir_names
-            if dir_flag:
-                dir_list.append(dir_names)
-    return dir_list
+class HtmlToPdf(object):
+    def __init__(self):
+        self.html_dir = os.path.dirname(os.path.realpath(__file__))
+        self.options = {
+            'header-html': 'header.htm',
+        }
 
+    def get_html_dir(self):
+        dir_list = []
+        for home, dirs, files in os.walk(self.html_dir):
+            # 获得所有文件夹
+            for dirname in dirs:
+                dir_names = os.path.join(home, dirname)
+                dir_flag = '未更新完' in dir_names or '已更新完' in dir_names
+                if dir_flag:
+                    dir_list.append(dir_names)
+        return dir_list
 
-def findAllHtmlFile(dir):
-    fileNames = []
-    for root, ds, fs in os.walk(dir):
-        for f in fs:
-            if '.html' in f:
-                fileNames.append(root + '\\' + f)
+    @classmethod
+    def get_html(cls, html_path):
+        fileNames = []
+        for root, ds, fs in os.walk(html_path):
+            for f in fs:
+                if '.html' in f:
+                    fileNames.append(root + '\\' + f)
+        return fileNames
 
-    return fileNames
+    @classmethod
+    def file_cmp(cls, file1, file2: str) -> int:
+        if int(str.split(os.path.split(file1)[1], '.')[0]) > int(str.split(os.path.split(file2)[1], '.')[0]):
+            return 1
+        elif int(str.split(os.path.split(file1)[1], '.')[0]) > int(str.split(os.path.split(file2)[1], '.')[0]):
+            return -1
+        else:
+            return 0
 
-
-def file_cmp(file1, file2: str) -> int:
-    if int(str.split(os.path.split(file1)[1], '.')[0]) > int(str.split(os.path.split(file2)[1], '.')[0]):
-        return 1
-    elif int(str.split(os.path.split(file1)[1], '.')[0]) > int(str.split(os.path.split(file2)[1], '.')[0]):
-        return -1
-    else:
-        return 0
+    def html_pdf(self, html_path):
+        str1 = os.path.realpath(html_path)
+        str2 = '\\'
+        pdf_path = str1[str1.rindex(str2) + 1:]
+        if pdf_path != '已更新完' and pdf_path != '未更新完':
+            html_files = self.get_html(html_path)
+            file_name = 'pdf/' + pdf_path + '.pdf'
+            print(file_name)
+            if os.path.exists(file_name):
+                os.remove(file_name)
+            htmls = sorted(html_files, key=cmp_to_key(self.file_cmp))
+            pdfkit.from_file(htmls, file_name, options=self.options, cover='statement.htm', cover_first=True)
 
 
 if __name__ == '__main__':
-    options = {
-        'header-html': 'header.htm',
-    }
-    parent_dir = os.path.dirname(os.path.realpath(__file__))
-    dir_list = getAllSub(parent_dir)
+    html_to_pdf = HtmlToPdf()
+    dir_list = html_to_pdf.get_html_dir()
     for html_path in dir_list:
-        html_files = findAllHtmlFile(html_path)
-        file_name = html_path + '.pdf'
-        if os.path.exists(file_name):
-            os.remove(file_name)
-        htmls = sorted(html_files, key=cmp_to_key(file_cmp))
-        pdfkit.from_file(htmls, file_name, options=options, cover='statement.htm', cover_first=True)
+        html_to_pdf.html_pdf(html_path)
